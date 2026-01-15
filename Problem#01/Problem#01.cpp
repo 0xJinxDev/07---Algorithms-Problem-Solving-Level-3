22f18ac92d03b15f7950dea679e9e0e898361aec
JinxDevs <JinxDevs@JINX>
2026-01-15 04:48:52 -0800

    add

--- Problem#01/Problem#01.cpp
+++ Problem#01/Problem#01.cpp
@@ -1,2 +1,45 @@
 //Write a program to fill 3x3 array with random numbers
 
+#include<iostream>
+#include<ctime>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To) {
+
+	return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int arr[3][3], short Rows, short Cols) {
+
+	for (short i = 0; i < Rows; i++) {
+
+		for (short j = 0; j < Cols; j++) {
+
+			arr[i][j] = RandomNumber(0, 10);
+		}
+	}
+
+}
+
+void PrintArray(const int arr[3][3], short Rows, short Cols) {
+
+	for (short i = 0; i < Rows; i++) {
+
+		for (short j = 0; j < Cols; j++) {
+
+			cout << setw(3) << arr[i][j];
+		}
+		cout << endl;
+	}
+
+}
+int main() {
+	srand((unsigned)time(NULL));
+	int arr[3][3];
+
+	FillArrayWithRandomNumbers(arr, 3, 3);
+	PrintArray(arr, 3, 3);
+
+}
---
+++ Problem#02/Problem#02.cpp
@@ -0,0 +1,68 @@
+//Write a program to sum each row in a matrix
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To) {
+
+	return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int arr[3][3], const short Rows, const short Cols) {
+
+	for (short i = 0; i < Rows; i++) {
+
+		for (short j = 0; j < Cols; j++) {
+
+			arr[i][j] = RandomNumber(1, 100);
+		}
+	}
+
+}
+
+
+int RowSum(const int arr[3][3], const short RowNumber, const short Cols) {
+
+	int sum = 0;
+
+	for (short j = 0; j < Cols; j++) {
+		sum += arr[RowNumber][j];
+	}
+
+	return sum;
+
+}
+
+void PrintSum(const int arr[3][3],const short Rows, const short Cols) {
+
+	for (short i = 0; i < Rows; i++) {
+
+		cout << "The sum of row " << i + 1 << " is " << RowSum(arr, i, Cols)<<endl;
+	}
+
+
+}
+void PrintArray(const int arr[3][3], const short Rows, const short Cols) {
+
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+
+			cout << setw(3) << arr[i][j];
+		}
+		cout << endl;
+	}
+
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+	int arr[3][3];
+
+	FillArrayWithRandomNumbers(arr, 3, 3);
+	PrintArray(arr, 3, 3);
+	PrintSum(arr,3,3);
+	return 0;
+}
